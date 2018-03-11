96479b4bdf7c39bfd427b7ca3722dc4ed553d744
Quinn <klassenq@gmail.com>
2018-03-10 22:49:17 -0800

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+
+
+var scene, camera, renderer;
+var material;
+var config = {
+    resolution: '512'
+};
+
+init();
+render();
+
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+    renderer = new THREE.WebGLRenderer( { canvas: canvas } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( config.resolution, config.resolution );
+    let d = document.getElementById( 'vertex_shader' );
+
+    var geometry = new THREE.PlaneBufferGeometry( 2.0, 2.0 );;
+    material = new THREE.RawShaderMaterial( {
+        vertexShader: document.getElementById( 'vertex_shader' ).textContent,
+        fragmentShader: document.getElementById( 'fragment_shader' ).textContent
+    } );
+    var mesh = new THREE.Mesh( geometry, material );
+    mesh.frustumCulled = false;
+    scene.add( mesh );
+
+    camera.position.z = 5;
+}
+
+function onWindowResize() {
+    if ( config.resolution === 'full' ) {
+        renderer.setSize( window.innerWidth, window.innerHeight );
+    } else {
+        renderer.setSize( config.resolution, config.resolution );
+    }
+    camera.aspect = canvas.width / canvas.height;
+    camera.updateProjectionMatrix();
+    material.uniforms.resolution.value.set( canvas.width, canvas.height );
+    material.uniforms.cameraProjectionMatrixInverse.value.getInverse( camera.projectionMatrix );
+}
+
+function render( time ) {
+    renderer.render( scene, camera );
+
+    requestAnimationFrame( render );
+}
