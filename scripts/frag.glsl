0a86321dc83b6fa23486609100a13374d225fb18
Quinn <klassenq@gmail.com>
2018-03-11 22:01:09 -0700

    More experimentations

--- scripts/frag.glsl
+++ scripts/frag.glsl
@@ -7,6 +7,9 @@ uniform vec3 cameraPosition;
 uniform mat4 cameraWorldMatrix;
 uniform mat4 cameraProjectionMatrixInverse;
 
+#define M_PI 3.1415926535897932384626433832795
+#define A 10.0
+
 vec2 cmpxcjg(in vec2 c) {
     return vec2(c.x, -c.y);
 }
@@ -31,7 +34,8 @@ float cmpxmag(in vec2 c) {
 }
 
 float cmpxarg(in vec2 z){
-    return atan(z.y,z.x);
+    float r = atan(z.y,z.x);
+    return r;
 }
 
 vec2 cmpxexp(in vec2 z){
@@ -42,7 +46,24 @@ vec2 cmpxexp(in vec2 z){
 }
 
 vec3 cmp2hsv(in vec2 z){
-    return vec3(cmpxarg(z), 1-pow(2,-cmpxmag(z)), 1.0);
+    return vec3(cmpxarg(z)/(2.0*M_PI), 1.0, 1.0-exp(-1.0*cmpxmag(z)) );
+}
+
+float hue2rgb(float f1, float f2, float hue) {
+    if (hue < 0.0)
+        hue += 1.0;
+    else if (hue > 1.0)
+        hue -= 1.0;
+    float res;
+    if ((6.0 * hue) < 1.0)
+        res = f1 + (f2 - f1) * 6.0 * hue;
+    else if ((2.0 * hue) < 1.0)
+        res = f2;
+    else if ((3.0 * hue) < 2.0)
+        res = f1 + (f2 - f1) * ((2.0 / 3.0) - hue) * 6.0;
+    else
+        res = f1;
+    return res;
 }
 
 vec3 hsl2rgb(vec3 hsl) {
@@ -70,6 +91,10 @@ vec3 hsl2rgb(vec3 hsl) {
 
 void main(void) {
     // screen position
-    vec2 screenPos = ( gl_FragCoord.xy * 2.0 - resolution ) / resolution;
-    gl_FragColor = vec4( hsl2rgb(cmp2hsv(screenPos)), 1.0 );
+    vec2 z = ( gl_FragCoord.xy * 2.0 - resolution ) / resolution;
+    vec2 w= cmpxexp(z);
+    for(int i = 0; i < 20; i++){
+        w = cmpxexp(cmpxmul(z,w));
+    }
+    gl_FragColor = vec4( hsl2rgb(cmp2hsv(w)), 1.0 );
 }
